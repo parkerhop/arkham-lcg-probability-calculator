4c3f7f0b0488ae24f10b947f05daa3749699f312
Parker Hopkins <peh2yf@virginia.edu>
2021-12-18 18:24:36 -0500

    initial commit

---
+++ js/calculator.js
@@ -0,0 +1,21 @@
+var counter = 0;
+
+var chaosBagTokens = [
+    {id: 'plusOne', quantity: 1, value: 1},
+    {id: 'zero', quantity: 2, value: 0},
+    {id: 'minusOne', quantity: 3, value: -1},
+    {id: 'minusTwo', quantity: 2, value: -2},
+    {id: 'minusThree', quantity: 1, value: -3},
+    {id: 'minusFour', quantity: 1, value: -4},
+    {id: 'cultist', quantity: 1, value: 'lookup'},
+    {id: 'autoFail', quantity: 1, value: -99},
+    {id: 'tablet', quantity: 1, value: 'lookup'},
+    {id: 'skull', quantity: 2, value: 'lookup'},
+    {id: 'elderThing', quantity: 1, value: 'lookup'},
+    {id: 'star', quantity: 1, value: 'lookup'}
+]
+
+function increment() {
+    console.log('Counter is at: ' + counter);
+    counter = counter + 1;
+}
