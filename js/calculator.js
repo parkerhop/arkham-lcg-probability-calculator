0b5344fb5fa12d12fd0b795232da5b769a38acc3
Parker Hopkins <peh2yf@virginia.edu>
2021-12-28 17:41:57 -0500

    create characters constant file and set input values based on character constants

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,5 @@
+const CHARACTERS = window.characterList;
+
 let chaosBagTokens = [
     {id: 'plusOne', quantity: 1, value: 1},
     {id: 'zero', quantity: 2, value: 0},
@@ -13,8 +15,11 @@ let chaosBagTokens = [
     {id: 'star', quantity: 1, value: 'lookup'}
 ]
 
+let players = [CHARACTERS.DAISY, CHARACTERS.ZOEY];
+
 function init() {
     togglePlayers($("#playerCount").val());
+    setCharacters(players);
 }
 
 function togglePlayers(playerCount) {
@@ -29,5 +34,14 @@ function togglePlayers(playerCount) {
     // Hide inactive players
     for (let index = playerCountNum + 1; index <= 4; index++) {
         $("div[data-index='" + index + "']").hide();
+    }
+}
+
+function setCharacters(characterList) {
+    for (let index = 1; index <= characterList.length; index++) {
+        var currentCharacter = characterList[index - 1];
+        $("input[name='characterName']").filter("input[data-index='" + index + "']").val(currentCharacter.characterName);
+        $("input[name='baseSkillValue']").filter("input[data-index='" + index + "']").val(currentCharacter.baseSkillValue);
+        $("input[name='starModifier']").filter("input[data-index='" + index + "']").val(currentCharacter.starModifier);
     }
 }
---
+++ js/characters.js
@@ -0,0 +1,12 @@
+window.characterList = {
+    "DAISY": {
+        "characterName": "Daisy",
+        "baseSkillValue": 2,
+        "starModifier": 0
+    },
+    "ZOEY" :{
+        "characterName": "Zoey",
+        "baseSkillValue": 2,
+        "starModifier": 1
+    }
+};
