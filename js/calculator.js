dc6b15a7f4f3d4cb17b82318241cd0c67405881c
Parker Hopkins <peh2yf@virginia.edu>
2021-12-28 19:07:27 -0500

    initial probability calculation function

--- js/calculator.js
+++ js/calculator.js
@@ -12,14 +12,20 @@ let chaosBagTokens = [
     {id: 'tablet', quantity: 1, value: 'lookup'},
     {id: 'skull', quantity: 2, value: 'lookup'},
     {id: 'elderThing', quantity: 1, value: 'lookup'},
-    {id: 'star', quantity: 1, value: 'lookup'}
+    // Use different value for star modifier since its lookup value is unique to each character
+    {id: 'star', quantity: 1, value: 'starModifier'}
 ]
 
 let players = [CHARACTERS.DAISY, CHARACTERS.ZOEY];
 
 function init() {
+    // Initialize player inputs
     togglePlayers($("#playerCount").val());
     setCharacters(players);
+
+    // Add onsubmit handler to calculate odds
+    let form = document.getElementById('calculatorForm');
+    form.onsubmit = calculate;
 }
 
 function togglePlayers(playerCount) {
@@ -44,4 +50,57 @@ function setCharacters(characterList) {
         $("input[name='baseSkillValue']").filter("input[data-index='" + index + "']").val(currentCharacter.baseSkillValue);
         $("input[name='starModifier']").filter("input[data-index='" + index + "']").val(currentCharacter.starModifier);
     }
+}
+
+function calculate(event) {
+    // If the form is valid, call preventDefault() to prevent page reloading
+    // Otherwise, allow default behavior, which will display any validation errors
+    let form = document.getElementById('calculatorForm');
+    if(form.checkValidity()){
+        event.preventDefault();
+    }
+
+    let playerCount = $("#playerCount").val();
+    let successThreshold = $("#successThreshold").val();
+    let failureOdds = 1.0;
+    for (let index = 1; index <= playerCount; index++) {
+        let attempts = parseFloat($("input[name='skillCheckAttempts']").filter("input[data-index='" + index + "']").val());
+        if (attempts > 0) {
+            failureOdds = failureOdds * calculateFailureOddsForPlayer(index, successThreshold, attempts);
+        }
+        console.log("Failure odds: " + failureOdds);
+    }
+    let finalOdds = (100* (1.0 - failureOdds));
+    $("#results").text((Math.round(finalOdds * 100) / 100.0).toString() + "%");
+}
+
+function calculateFailureOddsForPlayer(playerIndex, successThreshold, attempts) {
+    console.log('Player index: ' + playerIndex);
+    let totalChaosTokens = 0.0;
+    let successChaosTokens = 0.0;
+    let baseSkillValue = $("input[name='baseSkillValue']").filter("input[data-index='" + playerIndex + "']").val();
+    chaosBagTokens.forEach(function (token) {
+        totalChaosTokens += token.quantity;
+        let modifier = 0;
+        if (token.value === 'starModifier') {
+            modifier = $("input[name='starModifier']").filter("input[data-index='" + playerIndex + "']").val();
+        } else if (token.value === 'lookup') {
+            modifier = $("#" + token.id + "Value").val();
+        } else {
+            modifier = token.value;
+        }
+        successChaosTokens += token.quantity * (isSuccess(successThreshold, baseSkillValue, modifier) ? 1 : 0);
+    });
+    console.log('Total chaos tokens: ' + totalChaosTokens);
+    console.log('Total success totkens: ' + successChaosTokens);
+    console.log('Attempts: ' + attempts);
+    let failureOddsPerAttempt = (totalChaosTokens - successChaosTokens) / totalChaosTokens;
+    console.log('Failure odds per attempt: ' + failureOddsPerAttempt);
+    console.log('The final odds: ' + Math.pow(failureOddsPerAttempt, attempts));
+    return Math.pow(failureOddsPerAttempt, attempts);
+}
+
+function isSuccess(successThreshold, baseSkillValue, modifier) {
+    console.log("Success threshold: " + successThreshold + "; Base skill value: " + baseSkillValue + "; Modifier: " + modifier);
+    return parseInt(successThreshold) <= parseInt(baseSkillValue) + parseInt(modifier);
 }
